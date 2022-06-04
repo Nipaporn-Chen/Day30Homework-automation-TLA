4b4e5873bafbb76bcf40b72882edeecce81bac3a
Nipaporn-Chen <amp6722.ja@gmail.com>
2022-06-03 21:32:48 -0400

    Modified DataProvider9,10

--- src/test/dataProvider/DataProviders10.java
+++ src/test/dataProvider/DataProviders10.java
@@ -1,5 +1,16 @@
 package dataProvider;
 
-public class DataProviders10 {
+import org.testng.annotations.DataProvider;
 
+public class DataProviders10 {
+    @DataProvider(name = "userInfo")
+    public Object[][] data() {
+        Object[][] data = new Object[1][5];
+        data[0][0] = "Rose";
+        data[0][1] = "Miller";
+        data[0][2] = "7894561230";
+        data[0][3] = "Rose_Miller@gmail.com";
+        data[0][4] = "Mentor";
+        return data;
+    }
 }
--- src/test/dataProvider/DataProviders9.java
+++ src/test/dataProvider/DataProviders9.java
@@ -6,11 +6,11 @@ public class DataProviders9 {
     @DataProvider(name = "userInfo")
     public Object[][] data() {
         Object[][] data = new Object[1][5];
-        data[1][0] = "Lilly";
-        data[1][1] = "Rae";
-        data[1][2] = "1234567890";
-        data[1][3] = "Lilly_Rae@gmail.com";
-        data[1][4] = "Instructor";
+        data[0][0] = "Lilly";
+        data[0][1] = "Rae";
+        data[0][2] = "1234567890";
+        data[0][3] = "Lilly_Rae@gmail.com";
+        data[0][4] = "Instructor";
 
         return data;
     }
