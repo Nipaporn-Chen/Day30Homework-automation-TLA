184114561e9c9552b63568b21caed0064a992edb
Nipaporn-Chen <amp6722.ja@gmail.com>
2022-06-03 21:30:33 -0400

    Added DataProviders9

--- src/test/dataProvider/DataProviders9.java
+++ src/test/dataProvider/DataProviders9.java
@@ -1,5 +1,17 @@
 package dataProvider;
 
-public class DataProviders9 {
+import org.testng.annotations.DataProvider;
 
+public class DataProviders9 {
+    @DataProvider(name = "userInfo")
+    public Object[][] data() {
+        Object[][] data = new Object[1][5];
+        data[1][0] = "Lilly";
+        data[1][1] = "Rae";
+        data[1][2] = "1234567890";
+        data[1][3] = "Lilly_Rae@gmail.com";
+        data[1][4] = "Instructor";
+
+        return data;
+    }
 }
